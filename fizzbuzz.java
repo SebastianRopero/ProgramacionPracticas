4d434a1cffba812747eabc8d1811934322392d4a
SebastianRopero <sdroperom@ufpso.edu.co>
2024-10-04 19:14:39 -0500

    EjercicioFizzbuzz

---
+++ fizzbuzz.java
@@ -0,0 +1,21 @@
+public class fizzbuzz {
+
+    public static void main(String[] args) {
+        int max = 100;
+
+        for (int i=0; i<=max; i++){
+            if (i%3==0 && i%5==0){
+                System.out.println("fizzbuzz");
+            } else if (i%3==0) {
+                System.out.println("fizz");
+            }else if (i%5==0) {
+                System.out.println("buzz");
+            } else{
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+}
